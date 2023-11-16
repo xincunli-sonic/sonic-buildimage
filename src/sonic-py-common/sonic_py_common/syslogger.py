c1a1813b1d8d8e60475b3cb0c10322f97c86d2a8
xincunli-sonic <xincun.li@microsoft.com>
2023-11-16 10:31:40 -0800

    add back sysloger for lower case file name.

---
+++ src/sonic-py-common/sonic_py_common/syslogger.py
@@ -0,0 +1,60 @@
+import os
+import socket
+import sys
+import logging
+from logging.handlers import SysLogHandler
+
+
+class SysLogger:
+    """
+    SysLogger class for Python applications using SysLogHandler
+    """
+
+    DEFAULT_LOG_FACILITY = SysLogHandler.LOG_USER
+    DEFAULT_LOG_LEVEL = SysLogHandler.LOG_INFO
+
+    def __init__(self, log_identifier=None, log_facility=DEFAULT_LOG_FACILITY, log_level=DEFAULT_LOG_LEVEL):
+        if log_identifier is None:
+            log_identifier = os.path.basename(sys.argv[0])
+
+        # Initialize SysLogger
+        self.logger = logging.getLogger(log_identifier)
+        self.logger.setLevel(log_level)
+        handler = SysLogHandler(address="/dev/log", facility=log_facility, socktype=socket.SOCK_DGRAM)
+        formatter = logging.Formatter("%(name)s: %(message)s")
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+
+        # Set the default minimum log priority to 'LOG_DEBUG'
+        self.set_min_log_priority('LOG_DEBUG')
+
+    def set_min_log_priority(self, priority):
+        """
+        Sets the minimum log priority level. All log messages
+        with a priority lower than this will not be logged
+        """
+        self._min_log_level = self.LOG_LEVEL_MAP[priority]
+
+    # Methods for logging messages
+    def log(self, priority, msg, also_print_to_console=False):
+        if log_level >= self._min_log_level:
+            self.logger.log(log_level, msg)
+
+        if also_print_to_console:
+            print(msg)
+
+    # Convenience methods
+    def log_error(self, msg, also_print_to_console=False):
+        self.log(logging.ERROR,, msg, also_print_to_console)
+
+    def log_warning(self, msg, also_print_to_console=False):
+        self.log(logging.WARNING, msg, also_print_to_console)
+
+    def log_notice(self, msg, also_print_to_console=False):
+        self.log(logging.INFO, msg, also_print_to_console)
+
+    def log_info(self, msg, also_print_to_console=False):
+        self.log(logging.INFO, msg, also_print_to_console)
+
+    def log_debug(self, msg, also_print_to_console=False):
+        self.log(logging.DEBUG, msg, also_print_to_console)
